1451884729c63e899ed0dfc177d3e4beace3deb9
Chris Priest <cp368202@ohiou.edu>
2009-09-03 01:54:25 -0400

    currency class work

--- currency/currency.py
+++ currency/currency.py
@@ -1,180 +1,146 @@
+import re
 from datetime import *
 from dateutil.relativedelta import *
 
-FAA_MEDICAL_DURATIONS = {                               #the time elapsed from the original exam date for each downgrade in calendar months
-                                        "over": (6, 12, 48),
-                                        "under": (12, 12, 60)
-                                }
+from logbook.models import Flight
+from records.models import NonFlight
 
+def translate_units(value):
+    try:
+        number, unit = re.match("^(\d+)([a-z]+)$", value).groups()
+    except:
+        raise ValueError("FL: Invalid unit formatting")
+    
+    number = int(number)
+        
+    if unit == "d":
+        return timedelta(days=number)
+        
+    elif unit == "m":
+        return timedelta(months=number)
 
-CURRENCY_DATA =
-    {   0:      ("","",""),
-                        1:      ("Flight Instructor",   "24", "cm", "30", "d"), # (name, duration, duration units, alert time, alert time units)
-                        2:      ("Flight Review",       "24", "cm", "30", "d"),
-                        3:      ("1st Class Medical"),
-                        4:      ("2nd Class Medical"),
-                        5:      ("3rd Class Medical"),
+    elif unit == "y":
+        return timedelta(years=e_number)
 
-                        100:    ("FAA Landings",        "90", "d", "10", "d"),
-                }
+    elif unit == "cm":
+        return relativedelta(months=+number + 1).replace(day=1) + relativedelta(days=-1)
+        
+    else:
+        raise ValueError("FL: Invalid unit formatting")
+            
+########################################
+########################################
 
-TODAY =                 date.today()
-
-
-
-##############
-
-class currency():
-    id              =       0                       #the ref number for this currency routine
-    title           =       CURRENCY_DATA[id][0]    #human readable title
-    status          =       3                       #ref number for status, 0=expired, 1=current, 2=almost, 3=never
-    start_date      =       date(500,07,21)         #date of event currency is based from, eg date of 3rd to last landing
-    expire_date     =       date(501,07,21)         #date that the currency expires
-    expired_since   =       date(501,07,22)         #first day of expiration
-
-    def get_status(self):
+class FAA_Currency(object):
+                           
+    CURRENCY_DATA = {
+                        "flight_instructor":   ("24cm", "30d"),        # (name, duration, alert time)
+                        "landings":            ("90d", "10d"),
+                        "flight_review":       ("24cm", "30d"),
+                        "medical_over":        ("6cm", "12cm", "48cm"),
+                        "medical_under":       ("12cm", "12cm", "60cm"),         # the time elapsed from the original exam date for each downgrade in calendar months
+                    }
+                    
+    def __init__(self, user):
+        self.user=user
+                    
+    def determine(self, method, start_date):
         """determine if the current date is before, after, or in the alert timeframe."""
+        
+        TODAY = date.today()
 
-        expire_time     =       CURRENCY_DATA[self.id][1]
-        expire_units    =       CURRENCY_DATA[self.id][2]
-
-        if expire_units == "d":
-            expire_delta    =       timedelta(days=expire_time)
-
-        if expire_units == "m":
-            expire_delta    =       timedelta(months=expire_time)
-
-        if expire_units == "y":
-            expire_delta    =       timedelta(years=expire_time)
-
-        if expire_units == "cm":        #calendar months
-            expire_delta    =       relativedelta(months=+expire_time + 1).replace(day=1) + relativedelta(days=-1)
-
-        ###############################################################
-
-        alert_time              =       CURRENCY_DATA[self.id][3]
-        alert_units             =       CURRENCY_DATA[self.id][4]
-
-        if alert_units == "d":
-            alert_delta     =       timedelta(days=alert_time)
-
-        if alert_units == "m":
-            alert_delta     =       timedelta(months=alert_time)
-
-        if alert_units == "y":
-            alert_delta     =       timedelta(years=alert_time)
-
-        if alert_units == "cm": #calendar months
-            alert_delta     =       relativedelta(months=+expire_time + 1).replace(day=1) + relativedelta(days=-1)
-
-        ####
-
-        if self.id > 100:                                                               # landing currencies
-            self.expire_date = self.start_date + relativedelta(days=+expire_time)
-        else:
-            self.expire_date = self.start_date + expire_delta
-
-        ####
+        expire_time   =   self.CURRENCY_DATA[method][0]     #get the alert and expire times based on the master dict
+        alert_time    =   self.CURRENCY_DATA[method][1]
+        
+        expire_date = translate_units(expire_time) + start_date
+        alert_date = translate_units(expire_time) + start_date
+        
 
         if TODAY > expire_date:                                 #today is later than expire date, EXPIRED
-            self.status = 0
+            return "EXPIRED"
 
         elif TODAY < expire_date and TODAY > alert_delta:       #today is later than alert, but not past expired date, ALERT
-            self.status = 2
+            return "ALERT"
 
         elif TODAY < expire_date and TODAY < alert_delta:       #today is before expire date, and before alert date, CURRENT
-            self.status = 1
+            return "CURRENT"
+        
+        else:
+            assert False
 
-
-
-        #if expire_date > TODAY + alert_delta:          #
-        #       self.status = 1
-
-        #if expire_date < TODAY + alert_delta:
-        #       self.status = 2
-
-        #if expire_date < TODAY:
-        #       status = 0
-        #       self.expire_date += relativedelta(days=+1)
-
-        return
-
-##############
-
-class FAA_flight_review(currency):
-
-    id = 2
-
-    def determine(self):
+    def flight_review(self):
 
         try:
             checkride_date = Flight.objects.filter(user=self.user, pilot_checkride=True).values_list("date", flat=True).reverse()[0]
-        except:
+        except IndexError:
             checkride_date = None
 
         try:
             fr_date = Flight.objects.filter(user=self.user, flight_review=True).values_list("date", flat=True).reverse()[0]
-        except:
+        except IndexError:
             fr_date = None
 
         ############
 
-        if fr_date == None and checkride_date == None:                          # no checkrides nor flight reviews in database
-            return
+        if not fr_date and not checkride_date:
+            return "NEVER"
 
-        elif fr_date == None and not checkride_date == None:                    #no flight review, but a pilot checkride, use the checkride date
-            self.start_date = checkride_date
+        elif checkride_date and not fr_date:
+            start_date = checkride_date
 
-        elif checkride_date == None and not fr_date == None:                    #no checkride, but there is a flight review, use the flight review date
-            self.start_date = fr_date
+        elif fr_date and not checkride_date:
+            start_date = fr_date
 
-        elif checkride_date > fr_date:                                          #if there are both checkrides and flight reviews, use the one with the latest date
-            self.start_date = checkride_date
+        elif checkride_date > fr_date:
+            start_date = checkride_date
 
         elif checkride_date < fr_date:
-            self.start_date = fr_date
+            start_date = fr_date
 
         ################################################################################
 
-        self.get_status()
+        return self.determine("flight_review", start_date)
 
-        return
+        
 
 
-class FAA_flight_instructor(currency):
-
-    id = 1
-
-    def determine(self):
+    def flight_instructor(self):
         try:
             checkride_date = Flight.objects.filter(user=self.user, cfi_checkride=True).values_list("date", flat=True).reverse()[0]
-        except:
+        except IndexError:
             checkride_date = None
 
         try:
             refresher_date = NonFlight.objects.filter(user=self.user, non_flying=4).values_list("date", flat=True).reverse()[0]
-        except:
+        except IndexError:
             refresher_date = None
 
         ############
 
-        if refresher_date == None and checkride_date == None:           # no checkrides nor flight reviews in database, return "never"
-            return
+        if not refresher_date and not checkride_date:           # no checkrides nor flight reviews in database, return "never"
+            return "NEVER"
 
-        elif refresher_date == None and not checkride_date == None:
-            self.start_date = checkride_date
+        elif checkride_date and not refresher_date:
+            start_date = checkride_date
 
-        elif checkride_date == None and not refresher_date == None:
-            self.start_date = refresher_date
+        elif refresher_date and not checkride_date:
+            start_date = refresher_date
 
         elif checkride_date > refresher_date:
-            self.start_date = checkride_date
+            start_date = checkride_date
 
         elif checkride_date < refresher_date:
-            self.start_date = refresher_date
+            start_date = refresher_date
 
         ############
 
-        self.get_status()
+        return self.get_status("flight_instructor", start_date)
 
-        return
+
+
+
+
+
+
+
+
