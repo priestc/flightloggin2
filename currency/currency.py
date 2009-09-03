e09582f3327f8967b2a1b19e329c4157bfb22d3d
Chris Priest <cp368202@ohiou.edu>
2009-09-03 19:46:20 -0400

    FAA currency algorithms now complete

--- currency/currency.py
+++ currency/currency.py
@@ -4,6 +4,7 @@ from dateutil.relativedelta import *
 
 from django.db.models import Q
 
+from plane.models import Plane
 from logbook.models import Flight
 from records.models import NonFlight
 
@@ -22,9 +23,9 @@ def get_date(expire_time, start):
         return timedelta(months=number) + start
 
     elif unit == "y":
-        return timedelta(years=number) + start
+        return start + relativedelta(years=+number)
 
-    elif unit == "cm":
+    elif unit == "cm":  #calendar months
         return (start + relativedelta(months=+number + 1)).replace(day=1) + relativedelta(days=-1)
         
     else:
@@ -36,72 +37,86 @@ def get_date(expire_time, start):
 class FAA_Currency(object):
                            
     CURRENCY_DATA = {
-                        "flight_instructor":   ("24cm", "30d"),        # (name, duration, alert time)
+                        "40":                  ("40y", "30d"),
+                        
+                        "flight_instructor":   ("24cm", "30d"),        # (name: duration, alert time)
                         "landings":            ("90d", "10d"),
                         "flight_review":       ("24cm", "30d"),
-                        "medical_over":        ("6cm", "12cm", "48cm"),
-                        "medical_under":       ("12cm", "12cm", "60cm"),         # the time elapsed from the original exam date for each downgrade in calendar months
+                        
+                        "first_over":          ("6cm", "30d"),
+                        "second_over":         ("12cm", "30d"),
+                        "third_over":          ("48cm", "30d"),
+                        
+                        "first_under":         ("12cm", "30d"),         # the time elapsed from the original exam date for each downgrade in calendar months
+                        "second_under":        ("12cm", "30d"),
+                        "third_under":         ("60cm", "30d")
                     }
                     
-    def __init__(self, user):
+    medical_date = None
+    medical_class = None
+    over_40 = False
+                    
+    def __init__(self, user, today=None):
         self.user=user
+        
+        if not today:
+            self.TODAY = date.today()
                     
     def _determine(self, method, start_date):
-        """determine if the current date is before, after, or in the alert timeframe."""
+        """determine if the current date is before, after,
+        or in the expire timeframe, or the alert timeframe."""
         
-        TODAY = date.today()
-
         expire_time   =   self.CURRENCY_DATA[method][0]     #get the alert and expire times based on the master dict
         alert_time    =   self.CURRENCY_DATA[method][1]
         
         expire_date = get_date(expire_time, start_date)
         alert_date = get_date(expire_time, start_date)
 
-        if TODAY > expire_date:                                 #today is later than expire date, EXPIRED
+        if self.TODAY > expire_date:                                 #today is later than expire date, EXPIRED
             return ("EXPIRED", expire_date)
 
-        elif TODAY < expire_date and TODAY > alert_date:       #today is later than alert, but not past expired date, ALERT
+        elif self.TODAY < expire_date and self.TODAY > alert_date:   #today is later than alert, but not past expired date, ALERT
             return ("ALERT", expire_date)
 
-        elif TODAY < expire_date and TODAY < alert_date:       #today is before expire date, and before alert date, CURRENT
+        elif self.TODAY < expire_date and self.TODAY < alert_date:   #today is before expire date, and before alert date, CURRENT
             return ("CURRENT", expire_date)
         
         else:
             assert False
 
 
-    def day_landing(self, cat_class=0, tr=None, tail=False):
-        """Returns the date of the third to last day/night landing,
+    def landing(self, cat_class=0, tr=None, tail=False, night=False):
+        """Returns the date of the third to last day or night landing,
         and whether or not it qualifies the user to be current"""
         
-        last_three = Flight.objects.filter(user=self.user, plane__cat_class=cat_class).filter(Q(day_l__gte=1) | Q(night_l__gte=1)).order_by('-date').values('date', 'day_l', 'night_l')[:3]
-        
-        total = 0
-        for flight in last_three:
-            total += flight['night_l'] + flight['day_l']
-            if total >= 3:
-                start_date = flight['date']
-                break;
-                
-        if total < 3:
-            return ("NEVER", None, None)
+        if tr:
+            if not night:
+                last_three = Flight.objects.filter(user=self.user, plane__type=tr).filter(Q(day_l__gte=1) | Q(night_l__gte=1)).order_by('-date').values('date', 'day_l', 'night_l')[:3]
             
-        status, end_date = self._determine("landings", start_date)
-        
-        return (status, start_date, end_date)
-        
-        
-
-
-    def night_landing(self, cat_class=0, tr=None, tail=False):
-        """Returns the date of the third to last night landing,
-        and whether or not it qualifies the user to be current"""
-        
-        last_three = Flight.objects.filter(user=self.user, night_l__gte=1, plane__cat_class=cat_class).order_by('-date').values('date', 'night_l')[:3]
-
+            if night:
+                last_three = Flight.objects.filter(user=self.user, plane__type=tr, night_l__gte=1).order_by('-date').values('date', 'night_l')[:3]
+            
+        elif tail and cat_class > 0:
+            plane = Plane.objects.filter(user=self.user, cat_class=cat_class, tags__icontains="tailwheel")
+            
+            if not night:
+                last_three = Flight.objects.filter(user=self.user, plane__in=plane).filter(Q(day_l__gte=1) | Q(night_l__gte=1)).order_by('-date').values('date', 'day_l', 'night_l')[:3]
+                
+            if night:
+                last_three = Flight.objects.filter(user=self.user, plane__in=plane, night_l__gte=1).order_by('-date').values('date', 'night_l')[:3]
+            
+        else:
+            if not night:
+                last_three = Flight.objects.filter(user=self.user, plane__cat_class=cat_class).filter(Q(day_l__gte=1) | Q(night_l__gte=1)).order_by('-date').values('date', 'day_l', 'night_l')[:3]
+                
+            if night:
+                last_three = Flight.objects.filter(user=self.user, plane__cat_class=cat_class, night_l__gte=1).order_by('-date').values('date', 'day_l', 'night_l')[:3]
+ 
         total = 0
         for flight in last_three:
             total += flight['night_l']
+            if not night:
+                 total += flight['day_l']
             if total >= 3:
                 start_date = flight['date']
                 break;
@@ -112,11 +127,6 @@ class FAA_Currency(object):
         status, end_date = self._determine("landings", start_date)
         
         return (status, start_date, end_date)
-            
-        
-
-
-
 
 
     def flight_review(self):
@@ -154,8 +164,6 @@ class FAA_Currency(object):
 
         return (status, start_date, end_date)
 
-        
-
 
     def flight_instructor(self):
         try:
@@ -190,10 +198,82 @@ class FAA_Currency(object):
         status, end_date = self._determine("flight_review", start_date)
 
         return (status, start_date, end_date)
+        
+    
+    def _get_medical_info(self):
+        """Finds out whether the user is over 40 based on their profile,
+           also gets the last medical in their logbook by date and sets
+           some variables based on when that medical was made, and what class it was"""
+           
+        try:
+            dob = self.user.get_profile().dob            ## try to get the user's DOB from their profile, if no profile is made, assume they are over 40
+        except AttributeError:
+            dob = datetime.date(1915,7,21)
+            
+        status, end_date = self._determine("40", dob)
+        self.over_40 = status == "EXPIRED"               ## if this comes back expired, user is over 40
+
+        ###############
+            
+        try:
+            last_medical = NonFlight.objects.filter(non_flying__in=[1,2,3]).order_by('-date')[0]        ## 1,2,3 = FAA medical exam codes
+        except IndexError:
+            return              #no medicals in logbook, return
+            
+        self.medical_date = last_medical.date
+        self.medical_class = last_medical.non_flying
 
 
+    def first_class(self):
+    
+        if not self.medical_date or not self.over_40:
+            self._get_medical_info()
+            
+        if not self.medical_class or not self.medical_class == 1:           #if medical was not issued as a first, it can never be a first
+            return "NEVER"
+            
+        if self.over_40:
+            method = "first_over"
+        else:
+            method = "first_under"
+            
+            
+        status, end_date = self._determine(method, self.medical_date)
+        return (status, self.medical_date, end_date)
 
 
+    def second_class(self):
+    
+        if not self.medical_date or not self.over_40:
+            self._get_medical_info()
+
+        if not self.medical_class or self.medical_class == 3:           #if medical was issued as a third, it can never be a second
+            return "NEVER"
+            
+        if self.over_40:
+            method = "second_over"
+        else:
+            method = "second_under"
+
+        status, end_date = self._determine(method, self.medical_date)
+        return (status, self.medical_date, end_date)
+    
+    
+    def third_class(self):
+    
+        if not self.medical_date or not self.over_40:
+            self._get_medical_info()
+
+        if not self.medical_class:
+            return "NEVER"
+            
+        if self.over_40:
+            method = "third_over"
+        else:
+            method = "third_under"
+
+        status, end_date = self._determine(method, self.medical_date)
+        return (status, self.medical_date, end_date)     
 
 
 
