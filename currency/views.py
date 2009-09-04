f482cd8f171c625d96f323d67394cdd7b57c2d28
Chris Priest <cp368202@ohiou.edu>
2009-09-03 21:00:16 -0400

    renamed FAA currency file

--- currency/views.py
+++ currency/views.py
@@ -4,4 +4,12 @@ from is_shared import is_shared
 @render_to('currency.html')
 def currency(request, username):
     shared, display_user = is_shared(request, username)
+    
+    from FAA import FAA_Currency
+    
+    faa = FAA_Currency(display_user)
+    
+    
+    
+    
     return locals()
