c8a3dc9cefff984f7609a7f3b736aed7ac60eb88
Chris Priest <cp368202@ohiou.edu>
2009-09-19 17:26:22 -0400

    sig working fine now

--- sigs/views.py
+++ sigs/views.py
@@ -1,6 +1,6 @@
 import Image, ImageFont, ImageDraw
 from annoying.decorators import render_to
-from logbook.constants import FIELD_TITLES
+from logbook.constants import FIELD_TITLES, GRAPH_FIELDS
 
 @render_to('sigs.html')
 def sigs(request, shared, display_user):
@@ -23,17 +23,20 @@ def make_sig(request, shared, display_user, columns):
     
 class Sig(object):
     
-    number = {}
+    data = {}
     font_width = 6
+    line_height = 16
+    title_columns = []
+    max_title_width = 0
     
     def __init__(self, user, columns):
         
-        
-        height = len(columns) * 15
+        self.data = {}
+        height = len(columns) * self.line_height
         self.title_columns = [FIELD_TITLES[column] for column in columns]
         self.max_title_width = max(len(text) for text in self.title_columns)
         
-        width = (self.max_title_width + 8) * self.font_width
+        width = (self.max_title_width + 9) * self.font_width + 2
         
         self.user = user
         self.im = Image.new("RGBA", (width, height))
@@ -45,12 +48,17 @@ class Sig(object):
         
     def get_column(self, column):
         from logbook.models import Flight
-        self.number[column] = Flight.objects.user(self.user).agg(column)
+        self.data[column] = Flight.objects.user(self.user).agg(column)
     
     def put_all_columns(self):
-        for i,key in enumerate(self.number.keys()):
-            number = str(self.number[key])
-            self.draw.text((0, i*15), ("%*s: %s") % (self.max_title_width, FIELD_TITLES[key], number), font=self.font, fill='black')
+        i=0
+        for key in GRAPH_FIELDS:#self.number.keys()):
+            if key in self.data.keys(): # = str(self.number[key])
+                self.draw.text((1, (i*self.line_height)+1),
+                               ("%*s: %s") % (self.max_title_width+1, " " + FIELD_TITLES[key], self.data[key]),
+                               font=self.font,
+                               fill='black')
+                i += 1 #move the next line down one line width
     
     def output(self):
         self.put_all_columns()
