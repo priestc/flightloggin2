7a97eee9b8c5e1ed223550135669aafb791b61a5
Chris Priest <cp368202@ohiou.edu>
2009-10-07 01:59:32 -0700

    pdf now working fully, except for some superficial tweaking

--- pdf/views.py
+++ pdf/views.py
@@ -16,6 +16,7 @@ def pdf(request, display_user, shared):
     from reportlab.platypus import *
     from reportlab.lib.units import inch
     from reportlab.lib import colors
+    from reportlab.lib.pagesizes import landscape, letter
 
     s_date = dj_date_format(datetime.date.today(), 'Y-m-d')
     f_date = dj_date_format(datetime.date.today(), 'l, M d, Y')
@@ -24,7 +25,11 @@ def pdf(request, display_user, shared):
     response['Content-Disposition'] = 'attachment; filename=logbook%s.pdf' % s_date
     elements = []
     styles = getSampleStyleSheet()
-    doc = SimpleDocTemplate(response)
+    doc = SimpleDocTemplate(response, pagesize=landscape(letter), showBoundary=1,
+                            leftMargin=inch/4, rightMargin=inch/4,
+                            topMargin=inch/4, bottomMargin=inch/4,
+                            title="FlightLogg.in Logbook",
+                            author="FlightLogg.in")
      
     try:
         name = display_user.get_profile().username
@@ -34,11 +39,12 @@ def pdf(request, display_user, shared):
     elements.append(Paragraph("%s's Logbook" % name, styles['Heading1']))
     elements.append(Paragraph(f_date, styles['Normal']))
     
-    print_fields = ('date', 'plane', 'route', 'total_s', 'pic', 'sic', 'act_inst',
-                    'sim_inst', 'dual_r', 'dual_g', 'xc', 'night', 'day_l',
-                    'night_l', 'remarks')
-    
+    print_fields = ('date', 'plane', 'route', 'total_s', 'pic', 'sic', 'solo', 
+                    'act_inst', 'sim_inst', 'act_inst','xc','night','day_l',
+                    'night_l','person', 'r_remarks')
+                    
     flights = Flight.objects.all().select_related()
+    rows = flights.count() + 1  #+1 because of the header
     header = [FIELD_ABBV[f] for f in print_fields]
     data = [header,]
     for f in flights:
@@ -47,15 +53,23 @@ def pdf(request, display_user, shared):
             subdata.append(f.column(field))
         data.append(subdata)
         
-    ts = TableStyle([('FONTSIZE',       (0,0), (-1,-1), 8),
+    ts = TableStyle([('FONTSIZE',       (0,0), (-1,-1), 4),
+                     ('ALIGN',          (0,0), (-2,-1), 'CENTER'),
+                     ('VALIGN',         (0,0), (-1,-1), 'TOP'),
+                     ('ALIGN',          (0,-1),(-1,-1), 'LEFT'),
+                     ('LEFTPADDING',    (0,0), (-1,-1), 1),
+                     ('RIGHTPADDING',   (0,0), (-1,-1), 1),
+                     ('TOPPADDING',     (0,0), (-1,-1), 1),
+                     ('BOTTOMPADDING',  (0,0), (-1,-1), 1),
+                     ('ROWBACKGROUNDS', (0,0), (0,-1),  ['#FFFF00']),
                      ('ROWBACKGROUNDS', (0,0), (-1,-1), ['#DDFFEE', '#FFFFFF']),
-                     ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
-                     ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                     ('INNERGRID',      (0,0), (-1,-1), 0.25, colors.gray),
+                     ('BOX',            (0,0), (-1,-1), 0.25, colors.gray),
                     ])
      
     # Create the table with the necessary style, and add it to the
     # elements list.
-    table = Table(data, style=ts, repeatRows=1)
+    table = Table(data, rowHeights=rows*[0.10*inch], style=ts, repeatRows=1)
     
     
     
