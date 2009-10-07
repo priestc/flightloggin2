10b6ccb5a2f8409b1103b029fc881953d5d686ad
Chris Priest <cp368202@ohiou.edu>
2009-10-06 22:33:29 -0700

    working pdf demo

--- pdf/views.py
+++ pdf/views.py
@@ -1,2 +1,65 @@
-def pdf(request):
-    pass
+import datetime
+from django.utils.dateformat import format as dj_date_format
+
+from annoying.decorators import render_to
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.template import Context
+
+from logbook.models import Flight
+from logbook.constants import FIELD_ABBV
+
+
+#@render_to('pdf.html')
+def pdf(request, display_user, shared):
+    from reportlab.lib.styles import getSampleStyleSheet
+    from reportlab.platypus import *
+    from reportlab.lib.units import inch
+    from reportlab.lib import colors
+
+    s_date = dj_date_format(datetime.date.today(), 'Y-m-d')
+    f_date = dj_date_format(datetime.date.today(), 'l, M d, Y')
+    
+    response = HttpResponse(mimetype='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=logbook%s.pdf' % s_date
+    elements = []
+    styles = getSampleStyleSheet()
+    doc = SimpleDocTemplate(response)
+     
+    try:
+        name = display_user.get_profile().username
+    except:
+        name = display_user
+
+    elements.append(Paragraph("%s's Logbook" % name, styles['Heading1']))
+    elements.append(Paragraph(f_date, styles['Normal']))
+    
+    print_fields = ('date', 'plane', 'route', 'total_s', 'pic', 'sic', 'act_inst',
+                    'sim_inst', 'dual_r', 'dual_g', 'xc', 'night', 'day_l',
+                    'night_l', 'remarks')
+    
+    flights = Flight.objects.all().select_related()
+    header = [FIELD_ABBV[f] for f in print_fields]
+    data = [header,]
+    for f in flights:
+        subdata = []
+        for field in print_fields:
+            subdata.append(f.column(field))
+        data.append(subdata)
+        
+    ts = TableStyle([('FONTSIZE',       (0,0), (-1,-1), 8),
+                     ('ROWBACKGROUNDS', (0,0), (-1,-1), ['#DDFFEE', '#FFFFFF']),
+                     ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                     ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                    ])
+     
+    # Create the table with the necessary style, and add it to the
+    # elements list.
+    table = Table(data, style=ts, repeatRows=1)
+    
+    
+    
+    elements.append(table)
+     
+    doc.build(elements)
+    return response
