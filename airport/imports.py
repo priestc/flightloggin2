6b7e3e7eedf257e477dc6665016b1c7995c8dfad
Chris Priest <cp368202@ohiou.edu>
2009-10-08 14:40:08 -0700

    added working models and import feature

--- airport/imports.py
+++ airport/imports.py
@@ -159,3 +159,36 @@ def ic():   #import country
             print "error: " + code
 
     print "lines: " + str(count)
+    
+
+
+
+
+
+import os
+from django.contrib.gis.utils import LayerMapping
+from models import WorldBorders
+
+world_mapping = {
+    'fips' : 'FIPS',
+    'iso2' : 'ISO2',
+    'iso3' : 'ISO3',
+    'un' : 'UN',
+    'name' : 'NAME',
+    'area' : 'AREA',
+    'pop2005' : 'POP2005',
+    'region' : 'REGION',
+    'subregion' : 'SUBREGION',
+    'lon' : 'LON',
+    'lat' : 'LAT',
+    'mpoly' : 'MULTIPOLYGON',
+}
+
+world_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'worlddata/TM_WORLD_BORDERS-0.3.shp'))
+
+def importworld(verbose=True):
+    lm = LayerMapping(WorldBorders, world_shp, world_mapping,
+                      transform=False, encoding='iso-8859-1')
+
+    lm.save(strict=True, verbose=verbose)
+
--- airport/models.py
+++ airport/models.py
@@ -88,7 +88,7 @@ class Airport(Location):
 
         return ", ".join(ret)
     
-#####################################################################
+###############################################################################
     
 class Region(models.Model):
     code = models.CharField(max_length=48)
@@ -105,3 +105,35 @@ class Country(models.Model):
     
     def __unicode__(self):
         return self.name
+    
+##############################################################################    
+    
+    
+class WorldBorders(models.Model):
+    # Regular Django fields corresponding to the attributes in the
+    # world borders shapefile.
+    name = models.CharField(max_length=50)
+    area = models.IntegerField()
+    pop2005 = models.IntegerField('Population 2005')
+    fips = models.CharField('FIPS Code', max_length=2)
+    iso2 = models.CharField('2 Digit ISO', max_length=2)
+    iso3 = models.CharField('3 Digit ISO', max_length=3)
+    un = models.IntegerField('United Nations Code')
+    region = models.IntegerField('Region Code')
+    subregion = models.IntegerField('Sub-Region Code')
+    lon = models.FloatField()
+    lat = models.FloatField()
+
+    # GeoDjango-specific: a geometry field (MultiPolygonField), and
+    # overriding the default manager with a GeoManager instance.
+    mpoly = models.MultiPolygonField()
+    objects = models.GeoManager()
+
+    # So the model is pluralized correctly in the admin.
+    class Meta:
+        verbose_name_plural = "World Borders"
+
+    # Returns the string representation of the model.
+    def __unicode__(self):
+        return self.name
+
