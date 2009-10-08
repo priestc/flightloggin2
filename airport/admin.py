1a075fc9158e30d6a4b488a8c6818e63ca8f255e
Chris Priest <cp368202@ohiou.edu>
2009-10-08 14:50:50 -0700

    added admin

--- airport/admin.py
+++ airport/admin.py
@@ -1,5 +1,5 @@
 from django.contrib.gis import admin
-from models import Airport, Custom, Navaid
+from models import Airport, Custom, Navaid, WorldBorders
 
 class AirportAdmin(admin.GeoModelAdmin):
     list_display = ('identifier', 'name', 'country', 'region', 'municipality',)
@@ -13,6 +13,10 @@ class NavaidAdmin(admin.GeoModelAdmin):
     list_display = ('identifier', 'name', 'type',)
     search_fields = ('identifier', 'name',)
 
+class WorldBordersAdmin(admin.GeoModelAdmin):
+    search_fields = ('name',)
+
 admin.site.register(Navaid, NavaidAdmin)
 admin.site.register(Custom, CustomAdmin)
 admin.site.register(Airport, AirportAdmin)
+admin.site.register(WorldBorders, WorldBordersAdmin)
