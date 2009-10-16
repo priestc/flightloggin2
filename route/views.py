137d4308eb6d5841c2d1124f4bc1e90354675296
Chris Priest <cp368202@ohiou.edu>
2009-10-16 12:56:38 -0400

    assert false when trying to recalc and not logged in to admin account

--- route/views.py
+++ route/views.py
@@ -1,21 +1,29 @@
-from django.contrib.auth.decorators import login_required, permission_required
+
 from django.http import HttpResponse
 from models import Route
 
 
-@permission_required('auth.is_staff')
+
 def del_routes(request):
+    if not request.user.is_staff:
+        assert False
+        
     count=Route.objects.filter(flight__pk__isnull=True).count()
     Route.objects.filter(flight__pk__isnull=True).delete()
     return HttpResponse("%s routes deleted" % count)
 
-@permission_required('auth.is_staff')
+
 def easy_recalc_routes(request):
+    if not request.user.is_staff:
+        assert False
+        
     count = Route.easy_render_all()
     return HttpResponse("%s routes 'easy' recalculated" % count)
 
-@permission_required('auth.is_staff')
+
 def hard_recalc_routes(request):
+    if not request.user.is_staff:
+        assert False
     from django.contrib.auth.models import User
     for u in User.objects.all():
         count += Route.hard_render_all(user=u)
