d7f84aaabf680f3356ec41cfc445814ff0a26a53
Chris Priest <cp368202@ohiou.edu>
2009-10-17 11:25:29 -0400

    turn off a lot of columns by default

--- profile/admin.py
+++ profile/admin.py
@@ -3,7 +3,7 @@ from models import *
 
 class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user',)
-    list_display = ('user', 'date_registered', 'share', 'real_name',
+    list_display = ('user', 'date_registered', 'style', 'share', 'real_name',
                     'date_format', 'adminlink')
 
 admin.site.register(Profile, ProfileAdmin)
