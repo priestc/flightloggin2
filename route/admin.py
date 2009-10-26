fb1b332baf6332048e66791e3ef4712db1117f8b
Chris Priest <cp368202@ohiou.edu>
2009-10-25 20:24:32 -0400

    added hard render action to admin

--- route/admin.py
+++ route/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from models import *
 
+
+def hard_render(modeladmin, request, queryset):
+    for r in queryset:
+        r.hard_render()
+        
+hard_render.short_description = "Hard Render"
+
+####################################################
+
 class RouteBaseInline(admin.TabularInline):
     model = RouteBase
     extra = 3
@@ -9,6 +18,7 @@ class RouteBaseInline(admin.TabularInline):
 class RouteAdmin(admin.ModelAdmin):
     inlines = (RouteBaseInline,)
     search_fields = ('simple_rendered',)
+    actions = (hard_render, )
     list_display = ('simple_rendered', 'max_width_all', 'total_line_all',
                     'p2p', Route.owner)
 
@@ -17,7 +27,7 @@ class RouteBaseAdmin(admin.ModelAdmin):
     list_display = ('location', 'unknown', 'land', RouteBase.admin_loc_class,
                      RouteBase.owner)
     raw_id_fields = ('location', )
-
+    
 ####################################################
 
 admin.site.register(Route, RouteAdmin)
