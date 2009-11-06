be2f069e501febdb30d6414107e3962e5c9912a5
Chris Priest <cp368202@ohiou.edu>
2009-11-06 13:54:12 -0500

    moved more settings into local settings file (media paths)

--- settings.py
+++ settings.py
@@ -16,7 +16,6 @@ DEFAULT_FROM_EMAIL = "from@me.com"
 
 ## postgis setting for testing
 TEST_RUNNER='django.contrib.gis.tests.run_tests'
-POSTGIS_TEMPLATE='template_postgis'
 #OPENID_SSO_SERVER_URL = 'https://login.launchpad.net/'
 
 
@@ -62,21 +61,6 @@ SITE_ID = 1
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/chris/Websites/flightloggin/media'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/site-media'
-SITE_URL = "http://beta.flightlogg.in"
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
