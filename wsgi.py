03435cebbc361ab552b72fabcda1a3f8a1e9f7a6
Chris Priest <cp368202@ohiou.edu>
2009-11-05 22:34:38 -0500

    updated wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
 import os
-import sys
-
-sys.path = ['/srv', '/srv/flightloggin'] + sys.path
 os.environ['DJANGO_SETTINGS_MODULE'] = 'flightloggin.settings'
+os.environ['MPLCONFIGDIR'] = '/var/cache/mpl'
+
+import sys
+sys.path = ['/srv/', '/srv/flightloggin/'] + sys.path
+
+import site
+site.addsitedir('/srv/flightloggin/env/lib/python2.6/site-packages')
 
 from django.core.handlers.wsgi import WSGIHandler
 application = WSGIHandler()
