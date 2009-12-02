843d71442cba4b670383c62b87e286a6dc568be3
chris <chris@chris-comp.(none)>
2009-12-02 18:07:10 -0500

    added 'Agg' backend to histogram

--- graphs/histogram.py
+++ graphs/histogram.py
@@ -14,6 +14,8 @@ class Histogram(object):
                         
     def output(self):
         import numpy as np
+        import matplotlib
+        matplotlib.use('Agg')
         import matplotlib.pyplot as plt
         import matplotlib.mlab as mlab
 
