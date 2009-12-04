d07132722a5ae4bb198757a6f0da826cda1c11ac
chris <chris@chris-comp.(none)>
2009-12-03 23:26:44 -0500

    improved site_stats

--- site_stats/models.py
+++ site_stats/models.py
@@ -7,6 +7,58 @@ from django.db.models import Count, Sum, Avg
 from logbook.models import Flight
 from route.models import RouteBase, Route
 from plane.models import Plane
+from airport.models import Location
+
+class MostCommonPlane(object):
+    title = "??"
+    count = "??"
+    p = "??"
+      
+    def __call__(self):
+        derp = ""
+        for i,item in enumerate(self.p):
+            ident = item[self.title]
+            count = item[self.count]
+            derp += "%s. %s (%s)\n" % (i+1, ident, count)
+        
+        return derp
+
+class MostCommonManu(MostCommonPlane):
+    title = "manufacturer"
+    count = "c"
+    
+    def __init__(self):
+        self.p = Plane.objects\
+                      .exclude(flight=None)\
+                      .exclude(manufacturer='')\
+                      .values('manufacturer')\
+                      .distinct()\
+                      .annotate(c=Count('id'))\
+                      .order_by('-c')[:10]
+
+class MostCommonType(MostCommonPlane):
+    title = "type"
+    count = "c"
+    def __init__(self):
+        self.p = Plane.objects\
+                      .exclude(flight=None)\
+                      .exclude(type='')\
+                      .values('type')\
+                      .distinct()\
+                      .annotate(c=Count('id'))\
+                      .order_by('-c')[:10]
+    
+class MostCommonTail(MostCommonPlane):
+    title = "tailnumber"
+    count = "c"
+    def __init__(self):
+        self.p = Plane.objects\
+                      .exclude(flight=None)\
+                      .exclude(tailnumber='')\
+                      .values('tailnumber')\
+                      .distinct()\
+                      .annotate(c=Count('id'))\
+                      .order_by('-c')[:10]
 
 class StatDB(models.Model):
     dt = models.DateTimeField()
@@ -26,14 +78,9 @@ class StatDB(models.Model):
     avg_per_active =   models.FloatField(default=0, null=False)
     avg_duration =     models.FloatField(default=0, null=False)
     
-    most_common_tn =   models.CharField(max_length=10)
-    most_common_tnc =  models.PositiveIntegerField(default=0, null=False)
-    
-    most_common_ty =   models.CharField(max_length=10)
-    most_common_tyc =  models.PositiveIntegerField(default=0, null=False)
-    
-    most_common_manu = models.CharField(max_length=10)
-    most_common_manuc= models.PositiveIntegerField(default=0, null=False)
+    most_common_tail = models.TextField()
+    most_common_type = models.TextField()
+    most_common_manu = models.TextField()
     
     user_7_days =      models.PositiveIntegerField(default=0, null=False)
     num_7_days =       models.PositiveIntegerField(default=0, null=False)
@@ -42,122 +89,131 @@ class StatDB(models.Model):
     pwm_count =        models.PositiveIntegerField(default=0, null=False)
     pwm_hours =        models.FloatField(default=0, null=False)
     
+    #airport unique visitors
+    auv =              models.TextField(null=False)
+    
     class Meta:
         get_latest_by = 'dt'
 
-class Stat(object):    
-    
-    def save_to_db(self):
-        self.get_data()
-        
-        kwargs = {"dt": datetime.datetime.now()}
-        for i in ("users", "non_empty_users", "total_hours", "total_logged",
-                  "avg_per_active", "avg_duration", "unique_airports",
-                  "unique_countries", "route_earths", "total_dist",
-                  "most_common_tn", "most_common_tnc", "most_common_ty",
-                  "most_common_tyc", "most_common_manu", "most_common_manuc",
-                  "time_7_days", "num_7_days", "user_7_days", "pwm_hours",
-                  "pwm_count"):
-            kwargs.update({i: getattr(self, i)})
-        
-        sdb = StatDB(**kwargs)
-        sdb.save()
-        
+class Stat(object):
+
     def __init__(self):
-        self.users = User.objects.count()
-
-    def get_data(self):
-        #return if this function has already been called
-        if hasattr(self, "non_empty_users"):
-            return
-                          
-        self.non_empty_users = User.objects\
-                              .annotate(f=Count('flight'))\
-                              .filter(f__gte=1)\
-                              .count()
-                              
-        self.total_hours =     Flight.objects\
-                              .aggregate(t=Sum('total'))['t']
-                              
-        self.total_logged =    Flight.objects\
-                              .count()
-
-        self.unique_airports = RouteBase.objects\
-                              .values('location')\
-                              .distinct()\
-                              .count()
-                              
-        self.unique_countries = RouteBase.objects\
-                              .values('location__country')\
-                              .distinct()\
-                              .count()
-                              
-        EARTH = 21620.6641 #circumference of the earth in NM's
-        self.total_dist = Route.objects.aggregate(s=Sum('total_line_all'))['s']
-        self.route_earths = self.total_dist / EARTH
-        
-        self.avg_per_active = self.total_hours / self.non_empty_users
-        self.avg_duration = self.total_hours / self.total_logged
-        
-        p = Plane.objects\
-                 .exclude(flight=None)\
-                 .exclude(tailnumber='')\
-                 .values('tailnumber')\
-                 .distinct()\
-                 .annotate(c=Count('id'))\
-                 .order_by('-c')[0]
-            
-        self.most_common_tn = p['tailnumber']
-        self.most_common_tnc = p['c']
-                                      
-        p = Plane.objects\
-                 .exclude(flight=None)\
-                 .exclude(type='')\
-                 .values('type')\
-                 .distinct()\
-                 .annotate(c=Count('id'))\
-                 .order_by('-c')[0]
-                                
-        self.most_common_ty = p['type']
-        self.most_common_tyc = p['c']     
-                                 
-        p = Plane.objects\
-                 .exclude(flight=None)\
-                 .exclude(manufacturer='')\
-                 .values('manufacturer')\
-                 .distinct()\
-                 .annotate(c=Count('id'))\
-                 .order_by('-c')[0]
-                                
-        self.most_common_manu = p['manufacturer']
-        self.most_common_manuc = p['c']
-        
-        #seven days ago
-        sda = datetime.date.today() - datetime.timedelta(days=7)
-        
+        self.sda = datetime.date.today() - datetime.timedelta(days=7)
         #all flights in the past 7 days
-        fsd = Flight.objects.filter(date__gte=sda)
+        self.fsd = Flight.objects.filter(date__gte=self.sda)
         
-        self.time_7_days = fsd.aggregate(s=Sum('total'))['s']     
-        self.num_7_days = fsd.count()      
-        self.user_7_days = User.objects\
-                               .filter(flight__date__gte=sda)\
-                               .distinct()\
-                               .count()
-        
-        # calculate everyone's totals,
-        # also exclude users with less than 100 flights
-        pwm = User.objects.exclude(flight=None)\
+        #person with most...
+        self.pwm = User.objects.exclude(flight=None)\
                   .annotate(s=Sum('flight__total'))\
                   .annotate(c=Count('flight__id'))\
                   .annotate(a=Avg('flight__total'))\
                   .exclude(a__gte=30)
+                  
+    def save_to_db(self):
+        fields = ("users", "non_empty_users", "total_hours", "total_logged",
+                  "avg_per_active", "avg_duration", "unique_airports",
+                  "unique_countries", "total_dist", "route_earths",
+                  "most_common_tail", "most_common_type", "most_common_manu",
+                  "auv", "time_7_days", "num_7_days", "user_7_days",
+                  "pwm_hours", "pwm_count", )
+          
+        kwargs = {"dt": datetime.datetime.now()}
         
-        self.pwm_count = pwm.order_by('-c').values('c')[:1][0]['c']
-        self.pwm_hours = pwm.order_by('-s').values('s')[:1][0]['s']
+        for item in fields:
+            kwargs.update({item: getattr(self, "calc_%s" % item)()})
+        
+        sdb = StatDB(**kwargs)
+        sdb.save()
+    
+    #--------------------------------------------------------------------------
+    
+    def calc_pwm_hours(self):
+        return self.pwm.order_by('-s').values('s')[:1][0]['s']
+    
+    def calc_pwm_count(self):
+        return self.pwm.order_by('-c').values('c')[:1][0]['c']
+        
+    def calc_user_7_days(self):
+        return User.objects.filter(flight__date__gte=self.sda)\
+                   .distinct().count()
+    
+    def calc_num_7_days(self):  
+        return self.fsd.count()
+    
+    def calc_time_7_days(self):
+        return self.fsd.aggregate(s=Sum('total'))['s'] 
+                          
+    def calc_auv(self):
+        qs = Location.objects\
+                     .annotate(u=Count('routebase__route__flight__user',
+                               distinct=True))\
+                     .order_by('-u')\
+                     .values('identifier', 'u')[:10]
+        
+        foo = ""
+        for i,item in enumerate(qs):
+            ident = item['identifier']
+            count = item['u']
+            foo += "%s. %s (%s)\n" % (i+1, ident, count)
+        
+        return foo
+    
+    
+    calc_most_common_manu = MostCommonManu()
+    calc_most_common_type = MostCommonType()
+    calc_most_common_tail = MostCommonTail()
+    
+    def calc_route_earths(self):
+        """Must be called after calc_total_dist"""
+        EARTH = 21620.6641 #circumference of the earth in NM'
+        return self.total_dist / EARTH  
+    
+    def calc_total_dist(self):
+        self.total_dist = Route.objects.aggregate(s=Sum('total_line_all'))['s']
+        return self.total_dist
+    
+    def calc_unique_countries(self):
+        return RouteBase.objects\
+                        .values('location__country')\
+                        .distinct()\
+                        .count()
+
+    
+    
+    def calc_unique_countries(self):
+        return RouteBase.objects\
+                        .values('location__country')\
+                        .distinct()\
+                        .count()
+    
+    def calc_unique_airports(self):
+        return RouteBase.objects.values('location').distinct().count()
+    
+    def calc_avg_duration(self):
+        return self.total_hours / self.total_logged
+    
+    def calc_avg_per_active(self):
+        return self.total_hours / self.non_empty_users
+
+    def calc_total_logged(self):
+        self.total_logged = Flight.objects.count()
+        return self.total_logged
+
+    def calc_total_hours(self):
+        self.total_hours = Flight.objects.aggregate(t=Sum('total'))['t']
+        return self.total_hours
+        
+    def calc_non_empty_users(self):
+        self.non_empty_users = User.objects\
+                        .annotate(f=Count('flight')).filter(f__gte=1).count()
+        return self.non_empty_users
+
+    def calc_users(self):
+        return User.objects.count()
 
 
-        
+
+
     def openid(self):
         from django_openid_auth.models import UserOpenID
         
@@ -175,7 +231,7 @@ class Stat(object):
         
         self.others = self.users - (self.aol + self.my + self.yahoo + self.google)
         self.o_p = self.others / float(self.users) * 100
-        
+       
 #
 # install this crontab to get automatically updated stats
 #
