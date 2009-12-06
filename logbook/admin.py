2a3e27f9a1e1306f42f6a8f08a68cf4d97658751
chris <chris@chris-comp.(none)>
2009-12-05 19:10:20 -0500

    improved admin

--- logbook/admin.py
+++ logbook/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import *
 
 class FlightAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date', 'plane', 'route', 'remarks', )
+    list_display = ('user', 'date', 'total', 'plane', 'route', 'remarks', )
     search_fields = ('user', 'remarks')
     raw_id_fields = ('plane', 'route')
     #filter_horizontal = ('user', )
--- profile/admin.py
+++ profile/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import *
 
 class ProfileAdmin(admin.ModelAdmin):
-    search_fields = ('user',)
+    search_fields = ('user__username',)
     list_display = ('user', 'date_registered', 'style', 'real_name',
                     'date_format', 'adminlink')
 
