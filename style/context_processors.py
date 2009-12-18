e7c40e775ffc7efc909dca2978f375014ff51651
chris <chris@chris-comp.(none)>
2009-12-18 04:41:19 -0500

    fixed css context processor to work better with not logged in users

--- style/context_processors.py
+++ style/context_processors.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 
 def css_path(request):
-    try:
-        style = request.user.get_profile().style
-    except:
-        style = 1
-        
-    CSS_URL = settings.MEDIA_URL + "/css/style-" + str(style)
+    """ Return the path to the CSS directory based on the display user's
+        profile. if no display user, it falls back to request.user
+    """
+    u = getattr(request, "display_user", None) or request.user
+
+    from profile.models import Profile
+    style = Profile.get_for_user(u).style
+    
+    import os
+    CSS_URL = os.path.join(settings.MEDIA_URL, "css", "style-%s" % style)
          
     return {"CSS_URL": CSS_URL}
