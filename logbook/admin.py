707359f2c97f40aa733db2714b0041a0ba5cb118
chris <chris@chris-comp.(none)>
2010-01-17 19:38:45 -0500

    added better searching in logbook admin

--- logbook/admin.py
+++ logbook/admin.py
@@ -3,7 +3,7 @@ from models import *
 
 class FlightAdmin(admin.ModelAdmin):
     list_display = ('user', 'date', 'total', 'plane', 'route', 'remarks', )
-    search_fields = ('user', 'remarks')
+    search_fields = ('user__username', 'remarks')
     raw_id_fields = ('plane', 'route')
     #filter_horizontal = ('user', )
     
