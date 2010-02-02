8bac3574a9ee284672ed97b85468150335d0da1f
Chris Priest - linode <nbvfour@gmail.com>
2010-02-02 17:46:45 -0500

    facebook app work

--- facebook_app/views.py
+++ facebook_app/views.py
@@ -1,7 +1,7 @@
 from annoying.decorators import render_to
 import facebook.djangofb as facebook
 
-from django.models.db import Sum
+from django.db.models import Sum
 
 from profile.models import Profile
 from django.contrib.auth.models import User
@@ -12,27 +12,37 @@ from logbook.models import Flight
 def canvas(request):
     uid = request.facebook.uid
 
-    username = request.POST['username']
-    secret_key = request.POST['secret_key']
+    username = request.POST.get('username', None)
+    secret_key = request.POST.get('secret_key', None)
 
-    p = Profile.goon(user__username=username, secret_key=secret_key)
 
-    if p:
-        p.facebook_uid = uid
-        p.save()
-        message = "You are authenticated!"
-    else:
-        message = "There was an error :("
+    if username and secret_key:
+
+	p = Profile.goon(user__username=username, secret_key=secret_key)
+
+        if p:
+            p.facebook_uid = uid
+            p.save()
+            message = "You are authenticated!"
+        else:
+            message = "There was an error :("
 
     return locals()
 
-@render_to('facebook_app/canvas.fbml')
+@render_to('facebook_app/profile_tab.fbml')
 @facebook.require_login()
 def profile_tab(request):
     uid = request.facebook.uid
     
-    user = User(profile__facebook_uid=uid)
-    tt = Flight.objects.user(user).sim(False).aggregate(Sum('total'))
+    try:
+        user = User.objects.get(profile__facebook_uid=uid)
+    except User.DoesNotExist:
+        user = None
+
+    if user:
+        tt = Flight.objects.user(user).sim(False).aggregate(s=Sum('total'))['s']
+    else:
+        return canvas(request)
     
     return locals()
 
