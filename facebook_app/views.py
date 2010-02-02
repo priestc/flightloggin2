146ac75b9b9a897f232862666a41f7d5c3f89e85
Chris Priest - linode <nbvfour@gmail.com>
2010-02-02 16:48:12 -0500

    basic authentication in facebook app working

--- facebook_app/views.py
+++ facebook_app/views.py
@@ -1,10 +1,24 @@
 from annoying.decorators import render_to
 import facebook.djangofb as facebook
+from profile.models import Profile
 
 @render_to('facebook_app/canvas.fbml')
 @facebook.require_login()
 def canvas(request):
     uid = request.facebook.uid
+
+    username = request.POST['username']
+    secret_key = request.POST['secret_key']
+
+    p = Profile.goon(user__username=username, secret_key=secret_key)
+
+    if p:
+        p.facebook_uid = uid
+        p.save()
+        message = "You are authenticated!"
+    else:
+        message = "There was an error :("
+
     return locals()
 
 @render_to('facebook_app/canvas.fbml')
