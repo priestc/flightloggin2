1b180f634591f6e9528565f8a2e4b624ffb61a29
chris <chris@chris-comp.(none)>
2010-02-02 13:00:15 -0500

    facebook app small improvements

--- facebook_app/views.py
+++ facebook_app/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
-from django.views.generic.simple import direct_to_template
-
+from annoying.decorators import render_to
 import facebook.djangofb as facebook
 
+@render_to('facebook_app/canvas.fbml')
 @facebook.require_login()
 def canvas(request):
-    return direct_to_template(request,
-                              'facebook_app/canvas.fbml',
-                              extra_context={'uid': request.facebook.uid})
-
+    uid = request.facebook.uid
+    return locals()
