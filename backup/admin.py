1986f4e21a056204a0df842073fcc7eaa760747e
chris <chris@chris-comp.(none)>
2010-02-16 18:29:07 -0500

    show last 14 days in the userstoday admin page

--- backup/admin.py
+++ backup/admin.py
@@ -4,5 +4,6 @@ from models import UsersToday
 class UsersTodayAdmin(admin.ModelAdmin):
     list_display = ('date', 'users_count', 'usernames', 'email_usernames')
     raw_id_fields = ('logged_today', )
+    list_per_page = 14
 
 admin.site.register(UsersToday, UsersTodayAdmin)
