2d63c0157940135fada4afd7e597e935d2364907
chris <chris@chris-comp.(none)>
2010-02-19 23:29:13 -0500

    refactor milestones

--- milestones/views.py
+++ milestones/views.py
@@ -21,7 +21,40 @@ def smallbar(request, val, max_val):
     from small_progress_bar import SmallProgressBar
     return SmallProgressBar(float(val), float(max_val)).as_png()
 
+
+def determine(data):
+    """
+    Given a data dict, it goes through each item and creates a new dict
+    based on if the minimums are met
+    """
     
+    fails = 0
+    result = {}
+    for item,value in data.items():
+        
+        mine = value[0]
+        goal = value[1]
+               
+        result[item] = mine
+        result['goal_%s' % item] = goal
+
+        if float(mine) >= float(goal):
+            #the requirement is met
+            result['icon_%s' % item] = V
+        else:
+            result['icon_%s' % item] = X
+            fails += 1
+            
+    # if just one requirement is not met, overall good is false,
+    # did not meet milestone
+    if fails > 0:
+        result['overall'] = X
+    else:
+        result['overall'] = V
+    
+    return result
+
+
 def part135ifr(qs):
     """ Part 135 IFR minimums """
     
@@ -42,84 +75,60 @@ def part135ifr(qs):
     
     part135 = {}
     
-    my_numbers = {'total': qs.sim(False).agg('total'),
-                  'night': qs.sim(False).agg('night'),
-                  'p2p': qs.sim(False).agg('p2p'),
-                  'inst': inst}
-                  
-    goal_numbers = {'total': 1200,
-                    'night': 100,
-                    'p2p': 500,
-                    'inst': 75}
+    data = {
+                'total': (qs.sim(False).agg('total'), 1200),
+                'night': (qs.sim(False).agg('night'), 100),
+                'p2p':   (qs.sim(False).agg('p2p'),   500),
+                'inst':  (inst,                       75),
+           }
     
-    fails = 0
-    for item in ('total', 'night', 'p2p', 'inst'):
-        mine = my_numbers[item]
-        goal = goal_numbers[item]
-        
-        part135[item] = mine
-        part135['goal_%s' % item] = goal
+    return determine(data)
 
-        if float(mine) >= float(goal):
-            #the requirement is met
-            part135['icon_%s' % item] = V
-        else:
-            part135['icon_%s' % item] = X
-            fails += 1
+
+def p61_private(qs):
+    """
+    Determine Part 61.109 (private pilot) milestones
+    """
     
-    # if just one requirement is not met, overall good is false,
-    # did not meet milestone
-    if fails > 0:
-        part135['overall'] = X
-    else:
-        part135['overall'] = V
-
-    return part135
-
-
-
-
+    qs = qs.sim(False)
+    
+    from django.db.models import Count
+    # solo 3-point XC where max_width>50 and total dist is more than 150
+    long_xc = qs.solo().filter(route__max_width_land__gte=49)\
+                       .filter(route__total_line_all__gte=150)\
+                       .annotate(c=Count('route__routebase__land'))\
+                       .filter(c__gt=3)\
+                       .values('date')[:5]
+   
+   
+    ##################################################################
+    
+    data = {
+                "total":    (qs.agg('total'),         40),
+                "xc":       (qs.agg('p61_xc'),        3),
+                "dual_r":   (qs.agg('dual_r'),        20),
+                "solo":     (qs.agg('solo'),          10),
+                "night":    (qs.agg('night'),         3),
+                "night_l":  (qs.agg('night_l'),       10),
+                "solo_xc":  (qs.solo().agg('p61_xc'), 5),
+                "long_xc":  (long_xc,                 True),
+            }
+                 
+    return determine(data)
 
 
 def part135vfr(qs):
     """ Part 135 VFR minimums """
     
-    ############ part 135 IFR #################################################
+    qs = qs.sim(False)
     
-    part135 = {}
+    data = {
+               'total':     (qs.agg('total'),       500),
+               'night_p2p': (qs.p2p().agg('night'), 25),
+               'p2p':       (qs.agg('p2p'),         100),
+           }
     
-    my_numbers = {'total':     qs.sim(False).agg('total'),
-                  'night_p2p': qs.sim(False).p2p().agg('night'),
-                  'p2p':       qs.sim(False).agg('p2p'),}
-                  
-    goal_numbers = {'total': 500,
-                    'night_p2p': 25,
-                    'p2p': 100}
-    
-    fails = 0
-    for item in ('total', 'night_p2p', 'p2p'):
-        mine = my_numbers[item]
-        goal = goal_numbers[item]
-        
-        part135[item] = mine
-        part135['goal_%s' % item] = goal
-
-        if float(mine) >= float(goal):
-            #the requirement is met
-            part135['icon_%s' % item] = V
-        else:
-            part135['icon_%s' % item] = X
-            fails += 1
-    
-    # if just one requirement is not met, overall good is false,
-    # did not meet milestone
-    if fails > 0:
-        part135['overall'] = X
-    else:
-        part135['overall'] = V
-
-    return part135
-
+    return determine(data)
 
 
 
@@ -128,19 +137,21 @@ def atp_calc(qs):
     
     from logbook.models import Flight
     
-    ###########################################################################
-    
-    # only the first 25 hours of simulator instrument
+    # all real plane instrument
     plane_inst = qs.sim(False).agg('sim_inst', float=True) +\
                  qs.sim(False).agg('act_inst', float=True)
-                 
+    
+    # all simulator instrument             
     simulator_inst = qs.sim(True).agg('sim_inst', float=True)
     
+    # only the first 25 hours of simulator instrument can be used
     if simulator_inst > 25:
         simulator_inst = 25
     
     inst = simulator_inst + plane_inst
     
+    
+    # one hour for each night landings after 20, no more than 25 total
     extra_night=0
     night_l = qs.sim(False).agg('night_l', float=True)
     if night_l > 20:
@@ -150,47 +161,18 @@ def atp_calc(qs):
             extra_night = night_l
     
     night = qs.sim(False).agg('night', float=True)
+    disp_night = "%.1f" % float(night+extra_night)
     
-    ############ part 135 IFR #################################################
+    ################################################# 
     
-    atp = {}    
-    
-    my_numbers = {'total':     qs.sim(False).agg('total'),
-                  'night':     "%.1f" % float(night+extra_night),
-                  'atp_xc':    qs.sim(False).agg('atp_xc'),
-                  'inst':      inst,
-                  'pic':       qs.sim(False).agg('pic'),
-                  'pic_xc':    qs.sim(False).xc().agg('pic'),
-                  'pic_night': qs.sim(False).pic().agg('night'),}
-                  
-    goal_numbers = {'total': 1500,
-                    'night': 100,
-                    'atp_xc': 500,
-                    'inst': 75,
-                    'pic': 250,
-                    'pic_xc': 100,
-                    'pic_night': 25}
-    
-    fails = 0
-    for item in ('total', 'night', 'atp_xc', 'inst', 'pic', 'pic_xc', 'pic_night'):
-        mine = my_numbers[item]
-        goal = goal_numbers[item]
-        
-        atp[item] = mine
-        atp['goal_%s' % item] = goal
+    data = {
+                'total':     (qs.sim(False).agg('total'),       1500),
+                'night':     (disp_night,                       100),
+                'atp_xc':    (qs.sim(False).agg('atp_xc'),      500),
+                'inst':      (inst,                             75),
+                'pic':       (qs.sim(False).agg('pic'),         250),
+                'pic_xc':    (qs.sim(False).xc().agg('pic'),    100),
+                'pic_night': (qs.sim(False).pic().agg('night'), 25),
+            }
 
-        if float(mine) >= float(goal):
-            #the requirement is met
-            atp['icon_%s' % item] = V
-        else:
-            atp['icon_%s' % item] = X
-            fails += 1
-    
-    # if just one requirement is not met, overall good is false,
-    # did not meet milestone
-    if fails > 0:
-        atp['overall'] = X
-    else:
-        atp['overall'] = V
-
-    return atp
+    return determine(data)
