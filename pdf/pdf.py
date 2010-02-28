d38057113d462a5ce5280de2329cd9bd62c33ba0
Your Name <cp368202@ohiou.edu>
2010-02-28 17:44:40 -0500

    refactored PDF

---
+++ pdf/pdf.py
@@ -0,0 +1,129 @@
+import datetime
+from django.utils.dateformat import format
+
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.template import Context
+
+from logbook.models import Flight
+from logbook.constants import FIELD_ABBV
+
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Table, TableStyle, Paragraph, SimpleDocTemplate
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import landscape, letter
+
+class PDF(object):
+    
+    PRINT_FIELDS = ('date', 'plane', 'route', 'total_s', 'pic', 'sic', 'solo', 
+                    'act_inst', 'sim_inst', 'act_inst','xc','night','day_l',
+                    'night_l','person', 'r_remarks')
+                    
+    def __init__(self, user):
+        
+        self.s_date = format(datetime.date.today(), 'Y-m-d')
+        self.f_date = format(datetime.date.today(), 'l, M d, Y')
+        
+        self.user = user
+    
+        self.flights = Flight.objects\
+                             .user(user)\
+                             .order_by('date')\
+                             .select_related()
+                             
+        cd = 'attachment; filename=logbook{0}.pdf'.format(self.s_date)
+        self.response = HttpResponse(mimetype='application/pdf')
+        self.response['Content-Disposition'] = cd
+        
+        self.doc = SimpleDocTemplate(self.response,
+                                     pagesize=landscape(letter), 
+                                     showBoundary=1,
+                                     leftMargin=inch/4,
+                                     rightMargin=inch/4,
+                                     topMargin=inch/4,
+                                     bottomMargin=inch/4,
+                                     title="FlightLogg.in Logbook",
+                                     author="FlightLogg.in")
+                                     
+        
+    def define_style(self):
+        """
+        Define the styles that the rendered PDF will have
+        """
+        
+        from reportlab.lib import colors
+        
+        self.styles = getSampleStyleSheet()
+        
+        self.ts = TableStyle([
+                 ('FONTSIZE',       (0,0), (-1,-1), 4),
+                 ('ALIGN',          (0,0), (-2,-1), 'CENTER'),
+                 ('VALIGN',         (0,0), (-1,-1), 'TOP'),
+                 ('ALIGN',          (0,-1),(-1,-1), 'LEFT'),
+                 ('LEFTPADDING',    (0,0), (-1,-1), 1),
+                 ('RIGHTPADDING',   (0,0), (-1,-1), 1),
+                 ('TOPPADDING',     (0,0), (-1,-1), 1),
+                 ('BOTTOMPADDING',  (0,0), (-1,-1), 1),
+                 ('ROWBACKGROUNDS', (0,0), (0,-1),  ['#FFFF00']),
+                 ('ROWBACKGROUNDS', (0,0), (-1,-1), ['#DDFFEE', '#FFFFFF']),
+                 ('INNERGRID',      (0,0), (-1,-1), 0.25, colors.gray),
+                 ('BOX',            (0,0), (-1,-1), 0.25, colors.gray),
+                ])
+    
+    def make_elements(self):
+        """
+        Returns a list of all elements that will be rendered onto the document
+        """
+        
+        self.define_style()
+        
+        heading_style = self.styles['Heading1']
+        sub_heading_style = self.styles['Normal']
+        
+        real_name = self.user.get_profile().real_name
+        name = real_name or self.user.username
+        
+        elements = []
+        
+        big_heading = Paragraph("{0}'s Logbook".format(name), heading_style)
+        sub_heading = Paragraph(self.f_date, sub_heading_style)
+        
+        elements.append(big_heading)
+        elements.append(sub_heading)
+        
+        elements += self.construct_rows()
+        
+        return elements
+        
+    def construct_rows(self):
+        """
+        Create the table elements with the user's flight data
+        """
+        
+        elements = []
+        
+        rows = self.flights.count() + 1  #+1 because of the header
+        header = [FIELD_ABBV[f] for f in self.PRINT_FIELDS]
+        data = [header,]
+        for f in self.flights:
+            subdata = []
+            for field in self.PRINT_FIELDS:
+                subdata.append(f.column(field))
+            data.append(subdata)
+         
+        # Create the table with the necessary style, and add it to the
+        # elements list.
+        table = Table(data,
+                      rowHeights=rows*[0.10*inch],
+                      style=self.ts,
+                      repeatRows=1)
+        
+        elements.append(table)
+        
+        return elements
+    
+    def as_response(self):
+        elements = self.make_elements()
+        self.doc.build(elements)
+        return self.response
+        
--- pdf/views.py
+++ pdf/views.py
@@ -1,78 +1,9 @@
-import datetime
-from django.utils.dateformat import format as dj_date_format
-
-from annoying.decorators import render_to
-from django.template.loader import get_template
-from django.http import HttpResponse
-from django.template import Context
-
-from logbook.models import Flight
-from logbook.constants import FIELD_ABBV
 from share.decorator import no_share
+from pdf import PDF
 
 @no_share('logbook')
 def pdf(request):
-    from reportlab.lib.styles import getSampleStyleSheet
-    from reportlab.platypus import Table, TableStyle, Paragraph, SimpleDocTemplate
-    from reportlab.lib.units import inch
-    from reportlab.lib import colors
-    from reportlab.lib.pagesizes import landscape, letter
-
-    s_date = dj_date_format(datetime.date.today(), 'Y-m-d')
-    f_date = dj_date_format(datetime.date.today(), 'l, M d, Y')
     
-    response = HttpResponse(mimetype='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename=logbook%s.pdf' % s_date
-    elements = []
-    styles = getSampleStyleSheet()
-    doc = SimpleDocTemplate(response, pagesize=landscape(letter), showBoundary=1,
-                            leftMargin=inch/4, rightMargin=inch/4,
-                            topMargin=inch/4, bottomMargin=inch/4,
-                            title="FlightLogg.in Logbook",
-                            author="FlightLogg.in")
-     
-    name = request.display_user
-
-    elements.append(Paragraph("%s's Logbook" % name, styles['Heading1']))
-    elements.append(Paragraph(f_date, styles['Normal']))
+    pdf = PDF(request.display_user)
     
-    print_fields = ('date', 'plane', 'route', 'total_s', 'pic', 'sic', 'solo', 
-                    'act_inst', 'sim_inst', 'act_inst','xc','night','day_l',
-                    'night_l','person', 'r_remarks')
-                    
-    flights = Flight.objects\
-                    .user(request.display_user)\
-                    .order_by('date')\
-                    .select_related()
-                    
-    rows = flights.count() + 1  #+1 because of the header
-    header = [FIELD_ABBV[f] for f in print_fields]
-    data = [header,]
-    for f in flights:
-        subdata = []
-        for field in print_fields:
-            subdata.append(f.column(field))
-        data.append(subdata)
-        
-    ts = TableStyle([('FONTSIZE',       (0,0), (-1,-1), 4),
-                     ('ALIGN',          (0,0), (-2,-1), 'CENTER'),
-                     ('VALIGN',         (0,0), (-1,-1), 'TOP'),
-                     ('ALIGN',          (0,-1),(-1,-1), 'LEFT'),
-                     ('LEFTPADDING',    (0,0), (-1,-1), 1),
-                     ('RIGHTPADDING',   (0,0), (-1,-1), 1),
-                     ('TOPPADDING',     (0,0), (-1,-1), 1),
-                     ('BOTTOMPADDING',  (0,0), (-1,-1), 1),
-                     ('ROWBACKGROUNDS', (0,0), (0,-1),  ['#FFFF00']),
-                     ('ROWBACKGROUNDS', (0,0), (-1,-1), ['#DDFFEE', '#FFFFFF']),
-                     ('INNERGRID',      (0,0), (-1,-1), 0.25, colors.gray),
-                     ('BOX',            (0,0), (-1,-1), 0.25, colors.gray),
-                    ])
-     
-    # Create the table with the necessary style, and add it to the
-    # elements list.
-    table = Table(data, rowHeights=rows*[0.10*inch], style=ts, repeatRows=1) 
-    
-    elements.append(table)
-    
-    doc.build(elements)
-    return response
+    return pdf.as_response()
