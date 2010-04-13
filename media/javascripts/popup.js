fe8d9d850a26858426923f322add54cb46ba18f3
Your Name <cp368202@ohiou.edu>
2010-04-13 11:08:16 -0400

    cleaned up popup javascript

--- media/javascripts/popup.js
+++ media/javascripts/popup.js
@@ -12,89 +12,58 @@ function wipe_clean() {
 }
 
 function fire_popup() {
-
+    // a way to figure out how much the user has scrolled
+    // that works in both IE as well as the standards browsers
+    
     var scrolledX, scrolledY;
-	
-	if( self.pageYOffset )
-	{
-		scrolledX = self.pageXOffset;
-		scrolledY = self.pageYOffset;
-	}
-	else if( document.documentElement && document.documentElement.scrollTop )
-	{
-		scrolledX = document.documentElement.scrollLeft;
-		scrolledY = document.documentElement.scrollTop;
-	}
-	else if( document.body )
-	{
-		scrolledX = document.body.scrollLeft;
-		scrolledY = document.body.scrollTop;
-	}
+
+    if( self.pageYOffset ) {
+        scrolledX = self.pageXOffset;
+        scrolledY = self.pageYOffset;
+    }
+    
+    else if( document.documentElement && document.documentElement.scrollTop ) {
+        scrolledX = document.documentElement.scrollLeft;
+        scrolledY = document.documentElement.scrollTop;
+    }
+    
+    else if( document.body ) {
+        scrolledX = document.body.scrollLeft;
+        scrolledY = document.body.scrollTop;
+    }
 
     var p = $('#popup');
     p.centerScreen();
     p.draggable({ 
-		zIndex: 20,
-		cursor: 'move',
-		opacity: 1.0,
-		handle: '#dragbar'
-	});
-	
-	var t = p.css("top");
-	p.css("top", scrolledY + parseInt(t));
-	p.show();
-	
-}
+        zIndex: 20,
+        cursor: 'move',
+        opacity: 1.0,
+        handle: '#dragbar'
+    });
 
-function fire_popup2() {
-
-	// Determine how much the visitor had scrolled
-
-	var scrolledX, scrolledY;
-	
-	if( self.pageYOffset )
-	{
-		scrolledX = self.pageXOffset;
-		scrolledY = self.pageYOffset;
-	}
-	else if( document.documentElement && document.documentElement.scrollTop )
-	{
-		scrolledX = document.documentElement.scrollLeft;
-		scrolledY = document.documentElement.scrollTop;
-	}
-	else if( document.body )
-	{
-		scrolledX = document.body.scrollLeft;
-		scrolledY = document.body.scrollTop;
-	}
-
-	top = ((window.innerHeight - $('#popup').innerHeight()) / 2) + scrolledY;
-	left = (window.innerWidth - $('#popup').innerWidth()) / 2;
-
-	$('#popup').draggable({ 
-		zIndex: 20,
-		cursor: 'move',
-		opacity: 1.0,
-		handle: '#dragbar'
-	}).css("top", top).css("left", left).show();
+    var t = p.css("top");
+    p.css("top", scrolledY + parseInt(t));
+    p.show();
 }
 
 $(document).ready(function() {
 
-	$("#close_x").click(function(event) {
-		$("#popup").hide("slow");
-	});
-	
-	// fire popup if an error is detected
-	if( $("td#new_error_cell").text() != "" ) {     
-	    prepare_new();
-	    fire_popup();
-	}
-	
-	if( $("td#edit_error_cell").text() != "") {
-	    prepare_edit();   
-	    fire_popup();
-	}
+    $("#close_x").click(function(event) {
+        $("#popup").hide("slow");
+    });
+
+    // fire popup if an anything is in the error cell
+    // and it's the new flight that caused the error
+    if( $("td#new_error_cell").text() != "" ) {     
+        prepare_new();
+        fire_popup();
+    }
+
+    // ...it's the edit flight popup that caused the error
+    if( $("td#edit_error_cell").text() != "") {
+        prepare_edit();   
+        fire_popup();
+    }
 });
 
 
