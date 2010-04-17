7bf28fe06d5e29a29125c1a664cb373ecf3d4297
Your Name <cp368202@ohiou.edu>
2010-04-17 00:54:30 -0400

    added HelpItem model, templates and view to follow

--- main/admin.py
+++ main/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from models import *
 
 admin.site.register(NewsItem)
+admin.site.register(HelpItem)
--- main/models.py
+++ main/models.py
@@ -16,3 +16,13 @@ class NewsItem(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return  ('news', [])
+    
+class HelpItem(models.Model):
+    
+    title = models.TextField()
+    body = models.TextField()
+    category = models.CharField(max_length=32, blank=True)
+    order = models.SmallIntegerField(default=0)
+    
+    def __unicode__(self):
+        return self.title
