3d59d2de8907f2e8af521ec4ecb671b7003f7093
Your Name <cp368202@ohiou.edu>
2010-05-06 19:07:00 -0400

    refactored backup script to use amazon S3

---
+++ scripts/make-fixtures.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# dump the database, then send it to Amazon S3
+
+import datetime
+import os
+import time
+
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+from local_settings import AWS_SECRET_KEY, AWS_ACCESS_KEY
+
+# do the dump
+os.system("pg_dump --clean --format=c -U postgres logbook > this_dump")
+
+conn = S3Connection()
+bucket = conn.get_bucket('fl_dumps')
+
+k = Key(bucket)
+
+k.key = datetime.datetime.now().isoformat()
+
+#set the timestamp for later retrieval
+ts = time.mktime(datetime.datetime.now().timetuple())
+k.set_metadata('timestamp', ts)
+
+#upload file
+k.set_contents_from_filename('this_dump')
+
+#clean up
+os.remove('this_dump')
