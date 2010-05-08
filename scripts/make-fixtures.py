ea0f619082e0735f828d3206ac49e0d13c907fdf
Your Name <cp368202@ohiou.edu>
2010-05-07 22:15:10 -0400

    small tweak to make fixtures script

--- scripts/make-fixtures.py
+++ scripts/make-fixtures.py
@@ -10,8 +10,9 @@ from boto.s3.key import Key
 
 from local_settings import AWS_SECRET_KEY, AWS_ACCESS_KEY
 
-# do the dump
-os.system("pg_dump --clean --format=c -U postgres logbook > this_dump")
+# do the dump only if it already hasn't been done yet
+if not os.path.exists('this_dump'):
+    os.system("pg_dump --clean --format=c -U postgres logbook > this_dump")
 
 conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
 bucket = conn.get_bucket('fl_dumps')
@@ -22,7 +23,7 @@ k.key = datetime.datetime.now().isoformat()
 
 #set the timestamp for later retrieval
 ts = time.mktime(datetime.datetime.now().timetuple())
-k.set_metadata('timestamp', ts)
+k.set_metadata('timestamp', str(int(ts)))
 
 #upload file
 k.set_contents_from_filename('this_dump')
