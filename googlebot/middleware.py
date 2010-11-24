09db2942f72db267534c66e07f3049b830d0939c
nbv4 <cp368202@ohiou.edu>
2010-11-23 16:18:35 -0800

    Added yahoo's bot to the list of limited bot access

--- googlebot/middleware.py
+++ googlebot/middleware.py
@@ -10,10 +10,11 @@ class LimitBotsMiddleware(object):
         Limit googlebot to only see pages between the specefied start and end
         times
         """
-        
+        agent = request.META.get('HTTP_USER_AGENT', 'what').lower()
         now = datetime.datetime.now().hour
+
         in_interval = self.bots_start_hour < now < self.bots_stop_hour
-        is_googlebot = 'googlebot' in request.META.get('HTTP_USER_AGENT', 'what').lower()
-        
-        if is_googlebot and not in_interval:
+        is_googlebot = 'googlebot' in agent
+        is_yahoobot = 'yahoo! slurp' in agent
+        if (is_googlebot or is_yahoobot) and not in_interval:
             return HttpResponse("503 - Server Overloaded, come back later", status=503)
