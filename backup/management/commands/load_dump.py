6d27b5995d0888227c6088f34df61de00d97c66e
Chris Priest <cp368202@ohiou.edu>
2012-04-06 01:37:30 -0400

    added dump restore command

--- backup/management/commands/load_dump.py
+++ backup/management/commands/load_dump.py
@@ -1,29 +1,70 @@
+import logging
 import os
+from optparse import make_option
+
 from dateutil.parser import parse
 
 from boto.s3.connection import S3Connection
-from boto.s3.key import Key
 
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
 
+log = logging.getLogger(__name__)
+
+PROJECT_PATH = getattr(settings, 'PROJECT_PATH', None)
+TEMP_LOCATION = getattr(settings, 'TEMP_LOCATION', None)
+
 class Command(NoArgsCommand):
-
+    
+    option_list = NoArgsCommand.option_list + (
+        make_option(
+            '--dump',
+            '-d',
+            dest='dump',
+            help="Dump to perform",
+        ),
+    )
+    
     def handle(self, *args, **options):
+        DUMP_MANIFEST = getattr(settings, 'DUMP_MANIFEST')
+        dump = options['dump']
+        manifest = DUMP_MANIFEST[dump]                
+        c = S3Connection(settings.AWS_ACCESS_KEY, settings.AWS_SECRET_KEY)
+        bucket = c.get_bucket(manifest['s3-bucket'])
+        latest_key = self.get_latest(bucket)
+        key = bucket.get_key(latest_key)
+        path = self.get_save_path()
+        save_path = os.path.join(path, 'dump-{0}'.format(key.name))
         
-        conn = S3Connection(settings.AWS_ACCESS_KEY, settings.AWS_SECRET_KEY)
-        bucket = conn.get_bucket('fl_dumps')
+        if not os.path.exists(save_path):       
+            key.get_contents_to_filename(save_path)
+        else:
+            log.info('not downloading because it already has been downloaded')
+        
+        # put into postgres
+        cmd = 'pg_restore -d %s %s' % (manifest['database'], save_path)
+        os.system(cmd)
 
-        key = self.get_latest(bucket)
-        print key
+    def get_save_path(self):
+        """
+        Based on the settings provided, figure out where to save incoming dumps
+        """
+        if TEMP_LOCATION:
+            return TEMP_LOCATION
+            
+        if PROJECT_PATH:
+            return PROJECT_PATH
+        
+        return '.' # if no setting can be found, use current dir
 
-
-
-    def get_latest(bucket):
+    def get_latest(self, bucket):
         """
         Given a S3 bucket, return the key in that bucket named with the latest
         timestamp.
         """
-        d2 = parse(d1)
-        keys = [parse(k.name) for k in bucket.list()]
-        latest = sorted(keys)
+        keys = [{'dt': parse(k.name), 'string': k.name} for k in bucket.list()]
+        latest = sorted(keys, key=lambda x: x['dt'])[-1]
+        key = latest['string']
+        dt = latest['dt']
+        log.info("Using latest dump from: {0:%B %m, %y -- %X}".format(dt))
+        return key
