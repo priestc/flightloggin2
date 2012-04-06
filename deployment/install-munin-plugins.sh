8adf0f9a7be680d975e55a89e7cdd953fe0836c1
Chris Priest <cp368202@ohiou.edu>
2012-04-06 18:42:21 -0400

    added postgres plugins to munin install script

--- deployment/install-munin-plugins.sh
+++ deployment/install-munin-plugins.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+ln -s /usr/share/munin/plugins/postgres_bgwriter /etc/munin/plugins/postgres_bgwriter
 ln -s /usr/share/munin/plugins/postgres_cache_ /etc/munin/plugins/postgres_cache_logbook
 ln -s /usr/share/munin/plugins/postgres_connections_ /etc/munin/plugins/postgres_connections_logbook
 ln -s /usr/share/munin/plugins/postgres_locks_ /etc/munin/plugins/postgres_locks_logbook
+ln -s /usr/share/munin/plugins/postgres_querylength_ /etc/munin/plugins/postgres_querylength_logbook
+ln -s /usr/share/munin/plugins/postgres_scans_ /etc/munin/plugins/postgres_scans_logbook
+ln -s /usr/share/munin/plugins/postgres_size_ /etc/munin/plugins/postgres_size_logbook
+ln -s /usr/share/munin/plugins/postgres_transactions_ /etc/munin/plugins/postgres_transactions_logbook
+ln -s /usr/share/munin/plugins/postgres_xlog /etc/munin/plugins/postgres_xlog
