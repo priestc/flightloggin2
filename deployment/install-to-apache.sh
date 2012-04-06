ce89e59a6c37dff58a5d709ba24ac8435516d654
Chris Priest <cp368202@ohiou.edu>
2012-04-06 01:56:31 +0000

    added dirs creation to deployment scripts

--- deployment/install-to-apache.sh
+++ deployment/install-to-apache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ln -s /srv/flightloggin/deploy/flightloggin_apache.conf /etc/apache2/sites-enabled/000-default
+sudo ln -s /srv/flightloggin/deployment/flightloggin_apache.conf /etc/apache2/sites-enabled/000-default
---
+++ deployment/make-dirs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo mkdir /var/cache/mpl /var/fl-uploads
+sudo chown www-data:www-data /var/cache/mpl /var/fl-uploads
