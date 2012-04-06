38bae95ad91200da63f699c984ab005c9b81b610
Chris Priest <cp368202@ohiou.edu>
2012-04-06 22:38:49 +0000

    added munin install script

---
+++ deployment/install-munin-plugins.sh
@@ -0,0 +1,3 @@
+ln -s /usr/share/munin/plugins/postgres_cache_ /etc/munin/plugins/postgres_cache_logbook
+ln -s /usr/share/munin/plugins/postgres_connections_ /etc/munin/plugins/postgres_connections_logbook
+ln -s /usr/share/munin/plugins/postgres_locks_ /etc/munin/plugins/postgres_locks_logbook
