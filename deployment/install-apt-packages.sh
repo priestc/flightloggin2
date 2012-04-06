f2d7920e851d6351bb2e69579915cdec31fdadaa
Chris Priest <cp368202@ohiou.edu>
2012-04-06 22:38:30 +0000

    some misc fixe

--- deployment/install-apt-packages.sh
+++ deployment/install-apt-packages.sh
@@ -1,22 +1,22 @@
 sudo apt-get install \
-git\
-apache2\
-mercurial\
-bzr\
-subversion\
-postgres-9.1-postgis\
-python-pip\
-python-dev\
-virtualenv\
-libapache2-mod-wsgi\
-python-virtualenv\
-python-matplotlib\
-
-postgresql-contrib-9.1\
-proj\
-linpng12-dev\
-libfreetype6-dev\
-libpq-dev\
-libgeos-dev\
-libproj-dev\
-libjpeg62-dev\
+git \
+apache2 \
+mercurial \
+bzr \
+subversion \
+postgresql-9.1-postgis \
+python-pip \
+python-dev \
+python-virtualenv \
+libapache2-mod-wsgi \
+python-virtualenv \
+python-matplotlib \
+postgresql-contrib-9.1 \
+proj \
+linpng12-dev \
+libfreetype6-dev \
+libpq-dev \
+libgeos-dev \
+libproj-dev \
+libjpeg-dev \
+munin-node
