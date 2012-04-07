e9cebf73dec2d131ba8f86cbe4a6c551fe2759ae
Chris Priest <cp368202@ohiou.edu>
2012-04-06 20:08:34 -0400

    changed up apache config to reflect new static media in django

--- flightloggin/settings.py
+++ flightloggin/settings.py
@@ -9,17 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -80,9 +69,6 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'bezxnr4e+*7#rz-xjitl4hp%!7xo5_r8ysny(ba$c!$f-x%zsd'
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -100,7 +86,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'django_template.urls'
+ROOT_URLCONF = 'flightloggin.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'django_template.wsgi.application'
@@ -118,37 +104,15 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+################
+
+from settings_app import *
+from settings_private import *
+from settings_local import *
+
+INSTALLED_APPS = INSTALLED_APPS + DEV_APPS
--- flightloggin/settings_local.py
+++ flightloggin/settings_local.py
@@ -1,37 +1,3 @@
-DEBUG = False
-DEBUG_TOOLBAR = False
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'dongs'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'logbook',
-        'USER': 'chris',                  # Not used with sqlite3.
-        'PASSWORD': 'spatula',         # Not used with sqlite3.
-        'HOST': 'localhost',                    # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-RECAPTCHA_PRIVATE_KEY = '6LeOlLoSAAAAAAmlpMTXJHN0i_vpDOxekaDPrEoU'
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/srv/flightloggin/media'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/fl-media'
-SITE_URL = "http://flightlogg.in"
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/ad-media/'
-
 SERVER_EMAIL = 'info@fanmarkers.com'
 EMAIL_HOST = "localhost"
 EMAIL_HOST_PASSWORD = ""
@@ -39,18 +5,16 @@ EMAIL_HOST_USER = ""
 EMAIL_PORT = 25
 DEFAULT_FROM_EMAIL = "info@flightlogg.in"
 
-POSTGIS_TEMPLATE='template_postgis'
-
-ENV_DIR = '/srv/flightloggin/.fl_env'
-UPLOADS_DIR = '/var/fl-uploads'
-
-#the path where the state maps are stored
-BASE_MAP_PATH = '/var/www/states'
-
-#CACHE_BACKEND = 'db://logbook_cache'
-#CACHE_BACKEND = 'file:///var/tmp/django_cache'
-#CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
-#CACHE_BACKEND = 'main.pylibmcd://127.0.0.1:11000/'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'logbook',                      # Or path to database file if using sqlite3.
+        'USER': 'chris',                      # Not used with sqlite3.
+        'PASSWORD': 'spatula',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 #the url title where the states will show up, eg: http://flightlogg.in/states/user would be 'states'
 STATES_URL = 'states'
@@ -60,14 +24,33 @@ DEMO_USER_ID = 2
 UNKNOWN_PLANE_ID = 90
 COMMON_USER_ID = 1
 
-FACEBOOK_API_KEY = '8d04d4c1a5fae94c84b4a80b6f335cff'
-FACEBOOK_SECRET_KEY = '371872497b3372e740aed99c83fd2d7e'
+DEV_APPS = ['django_extensions', ]
 
-# for Amazon S3
-AWS_ACCESS_KEY = 'AKIAJDBRZGUZ2PE334MQ'
-AWS_SECRET_KEY = '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'
-
-
-GOOGLE_MAPS_KEY = "ABQIAAAAtDznsRv92g_KZ0HK9mBD5RS3ydai3R5pSwWU_IHTQxn79Z9awhQaWXs60I2yGfPRz5z-Bs_CTn5lMg"
-
-INSTALLED_APPS = ['django_extensions', ]
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
