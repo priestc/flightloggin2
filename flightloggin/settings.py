abc74b16509e8d1e2e06ae5efe55479312a8f62e
Chris Priest <cp368202@ohiou.edu>
2012-04-08 02:20:47 -0400

    added static_external as a static dir

--- flightloggin/settings.py
+++ flightloggin/settings.py
@@ -62,6 +62,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_external')
 )
 
 # List of finder classes that know how to find static files in
