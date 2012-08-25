71918b86c367b9e615a170bbd4d4d2cdae630df0
Chris Priest <cp368202@ohiou.edu>
2012-08-25 04:11:00 -0400

    fixed badges count context processor

--- badges/context_processors.py
+++ badges/context_processors.py
@@ -2,10 +2,16 @@ from models import AwardedBadge
 from django.db.models import Sum
 
 def badge_count(request):
+	
+	# displaying the total for the user that own the page
 	u = getattr(request, 'display_user', None)
-	bc = 0
-	if not u and request.user.is_authenticated():
+
+	logged_in = request.user.is_authenticated()
+	if not u and logged_in:
 		u = request.user
-		bc = AwardedBadge.objects.filter(user=u).aggregate(s=Sum('level'))['s']
+	if not u and not logged_in:
+		u = None
+	
+	bc = AwardedBadge.objects.filter(user=u).aggregate(s=Sum('level'))['s']
 	
 	return {'badges_count': bc or 0}
