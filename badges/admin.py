e18c497877bea0a2193ffaa4fd07d6182370cba9
Chris Priest <cp368202@ohiou.edu>
2012-11-08 02:46:48 -0500

    added raw_id_fields to badges admin

--- badges/admin.py
+++ badges/admin.py
@@ -5,5 +5,6 @@ class BadgeAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'awarded_date', 'level')
     search_fields = ('user', 'title')
     list_filter = ('title', )
+    raw_id_fields = ('awarded_flight', 'user')
 
 admin.site.register(AwardedBadge, BadgeAdmin)
