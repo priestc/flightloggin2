37596975f3972a9a4190f14561b0d015b689842a
Chris Priest <cp368202@ohiou.edu>
2012-11-13 18:47:25 -0500

    fixed small issue with SEO on the password reset page

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('landingpage.views',
     url(r'^$', "landingpage", name="landingpage"),
     url(r'new_login$', 'new_login', name='new_login'),
-    url(r'reset_password', 'reset_password', name='reset_password'),
+    url(r'reset_password$', 'reset_password', name='reset_password'),
     url(r'fb_registration_callback', 'fb_registration_callback', name='fb_registration_callback'),
 )
