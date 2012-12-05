a36bed8e4d6e7e44ab02920f76d1e661a0351023
Chris Priest <cp368202@ohiou.edu>
2012-12-05 13:27:26 -0500

    added baiduspider to bots middleware

--- googlebot/middleware.py
+++ googlebot/middleware.py
@@ -2,20 +2,21 @@ import os
 from django.http import HttpResponse
 
 class LimitBotsMiddleware(object):
-    bots_start_hour = 2
-    bots_stop_hour =  10
     
     def process_request(self, request):
         """
         Limit googlebot to only see pages between the specefied start and end
         times
         """
-        agent = request.META.get('HTTP_USER_AGENT', 'what').lower()
+        agent = request.META.get('HTTP_USER_AGENT', '').lower()
 
-        overloaded = os.getloadavg()[2] > 3
+        overloaded = os.getloadavg()[1] > 3
 
-        is_googlebot = 'googlebot' in agent
-        is_yahoobot = 'yahoo! slurp' in agent
-        is_bingbot = 'bingbot' in agent
-        if (is_googlebot or is_yahoobot or is_bingbot) and overloaded:
-            return HttpResponse("503 - Server Overloaded, come back later", status=503)
+        is_bot = False
+        for key in ['googlebot', 'yahoo! slurp', 'bingbot', 'Baiduspider']:
+            if key in agent:
+                is_bot = True
+                break
+
+        if is_bot and overloaded:
+            return HttpResponse("503 - Spider detected, server overloaded, come back later", status=503)
