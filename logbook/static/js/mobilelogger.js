08accab1ad9e02b50a995cbcb0548e68eb42b4dd
Chris Priest <cp368202@ohiou.edu>
2012-12-06 21:32:10 -0500

    added change theme function

--- logbook/static/js/mobilelogger.js
+++ logbook/static/js/mobilelogger.js
@@ -468,3 +468,32 @@ function prefill_purpose(data) {
         $('input[name=pic]').val(total);
     }
 }
+
+function change_theme(new_theme) {
+    if(new_theme == 'dark') {
+        theme = 'a';
+    } else if(new_theme == 'light') {
+        theme = 'c';
+    }
+    // change the theme of the application    
+    $.mobile.activePage.find('.ui-btn').not('.ui-li-divider')
+                       .removeClass('ui-btn-up-a ui-btn-up-b ui-btn-up-c ui-btn-up-d ui-btn-up-e ui-btn-hover-a ui-btn-hover-b ui-btn-hover-c ui-btn-hover-d ui-btn-hover-e')
+                       .addClass('ui-btn-up-' + theme)
+                       .attr('data-theme', theme);
+    
+    $.mobile.activePage.find('.ui-li-divider').each(function (index, obj) {
+        if ($(this).parent().attr('data-divider-theme') == 'undefined') {
+            $(this).removeClass('ui-bar-a ui-bar-b ui-bar-c ui-bar-d ui-bar-e')
+                   .addClass('ui-bar-b')
+                   .attr('data-theme', 'b');
+        }
+    });
+                       
+    $.mobile.activePage.find('.ui-header, .ui-footer')
+                       .removeClass('ui-bar-a ui-bar-b ui-bar-c ui-bar-d ui-bar-e')
+                       .addClass('ui-bar-' + theme)
+                       .attr('data-theme', theme);
+    $.mobile.activePage.removeClass('ui-body-a ui-body-b ui-body-c ui-body-d ui-body-e')
+                       .addClass('ui-body-' + theme)
+                       .attr('data-theme', theme);
+}
