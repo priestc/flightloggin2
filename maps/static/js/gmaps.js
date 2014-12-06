4b93195ba531d4f68e0803dc3ddda4c3c4880829
Chris Priest <nbvfour@gmail.com>
2014-12-06 14:05:10 -0800

    switched to google maps v3 (untested)

--- maps/static/js/gmaps.js
+++ maps/static/js/gmaps.js
@@ -1,58 +1,35 @@
-function initialize()
-{	
-	if (GBrowserIsCompatible())
-	{
-		map = new GMap2(document.getElementById("gmap"));
-		
-		var polyOptions = {geodesic:true};
-		
-		map.addMapType(G_PHYSICAL_MAP);
-		map.setMapType(G_PHYSICAL_MAP);
-		
-		map.setCenter(new GLatLng(10,0), 2);
-		
-		map.addControl(new GLargeMapControl3D());
-		map.addControl(new GHierarchicalMapTypeControl());
-		map.enableContinuousZoom();
-	}
-}
-
 var map;
 
 $(document).ready(function() {
-	
-	initialize();
-	
-	var kml = new GGeoXml(kml_url, function(){
-	
-		if (kml.loadedCorrectly())
-		{
-			map.addOverlay(kml);
-			kml.gotoDefaultViewport(map)
-		}
-		
-		if (map.getZoom() > 6)
-			map.setZoom(6)
-	});
+	var chicago = new google.maps.LatLng(41.875696,-87.624207);
+	var mapOptions = {
+		zoom: 11,
+		center: chicago
+	}
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-	
+	var ctaLayer = new google.maps.KmlLayer({url: kml_url});
+	ctaLayer.setMap(map);
+
+	if (map.getZoom() > 6) {
+		map.setZoom(6)
+	}
+
 });
 
 $(document).ready(function() {
 
     $('#toggle a').toggle(function() {
-    
+
         $(this).text("Hide Map Key");
         $('#key').show('slow');
-        
+
     },
     function() {
-    
+
         $(this).text("Show Map Key");
         $('#key').hide('slow');
-    
+
     });
 
 });
-
-window.onunload = GUnload;
